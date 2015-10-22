ee361294f1262ec295449e9c8350833141a5a316
Katherine Richer <katrich97@sympatico.ca>
2015-10-22 10:19:54 -0400

    testcomment

--- ClinicSys/src/group1/clinic/data/ClinicFileLoader.java
+++ ClinicSys/src/group1/clinic/data/ClinicFileLoader.java
@@ -25,6 +25,7 @@ import dw317.lib.medication.NDCMedication;
 /**
  * This class represents a File Loader.
  * It provides methods to load files containing Patients and Visits.
+ * testcomment
  * 
  * @author Katherine Richer
  * @author Erika Bourque
