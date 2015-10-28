0fc1685ad9d65ca81f58653b48cac7d9f34c2e18
Erika Bourque <erika.bourque94@gmail.com>
2015-10-28 13:33:49 -0400

    Added comments before pull to master.

--- ClinicSys/src/group1/clinic/data/ListPersistenceObject.java
+++ ClinicSys/src/group1/clinic/data/ListPersistenceObject.java
@@ -1,4 +1,3 @@
-<<<<<<< Upstream, based on branch 'master' of https://github.com/erikabourque/Java-III-Project.git
 /**
  * Defines a ListPersistenceObject type.
  */
@@ -16,16 +15,6 @@ import java.util.Queue;
  * @author	Uen Yi Hung (Cindy)
  * @version 25/10/2015
  */
-=======
-package group1.clinic.data;
-
-import dw317.clinic.business.interfaces.Patient;
-import dw317.clinic.business.interfaces.Visit;
-import java.io.IOException;
-import java.util.List;
-import java.util.Queue;
-
->>>>>>> 26631f6 ListPersistenceObject Interface
 public interface ListPersistenceObject {
 	List<Patient> getPatientDatabase();
 
