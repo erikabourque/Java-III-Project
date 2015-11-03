01cd357c353293711df1a08e34d0e2ed3b5c410b
1430468 <1430468@2F14-26.ad.dawsoncollege.qc.ca>
2015-11-03 09:25:26 -0500

    tre

--- ClinicSys/src/group1/clinic/data/VisitQueueDB.java
+++ ClinicSys/src/group1/clinic/data/VisitQueueDB.java
@@ -7,17 +7,39 @@ import group1.clinic.business.Priority;
 
 import java.io.IOException;
 import java.util.Optional;
-
+import java.util.List;
+import java.util.Queue;
+import dw317.clinic.business.interfaces.PatientVisitFactory;
 import dw317.clinic.business.interfaces.Visit;
 import dw317.clinic.data.NonExistingVisitException;
 import dw317.clinic.data.interfaces.VisitDAO;
 
 /**
- * @author 1430468
+ * @author Danieil Skrinikov
+ * @version 11/03/2015
  *
  */
 public class VisitQueueDB implements VisitDAO{
 
+	private List<Queue<Visit>> database;
+	private final ListPersistenceObject listPersistenceObject;
+	private final PatientVisitFactory factory;
+	
+	/*public VisitQueueDB(ListPersistenceObject listPersistenceObject){
+		
+		this.listPersistenceObject = listPersistenceObject;
+		//I do not understand how to get factory here. Plz help
+		database = listPersistenceObject.getVisitDatabase();
+		
+	}*/
+	
+	public VisitQueueDB(ListPersistenceObject listPersistenceObject, PatientVisitFactory factory){
+		
+		this.listPersistenceObject = listPersistenceObject;
+		this.factory = factory;
+		database = listPersistenceObject.getVisitDatabase();		
+	}
+	
 	@Override
 	public void add(Visit aVisit) {
 		// TODO Auto-generated method stub
@@ -44,8 +66,15 @@ public class VisitQueueDB implements VisitDAO{
 
 	@Override
 	public int size(Priority priority) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Object[] temp = database.toArray();
+		int count = 0;
+		for(int i = 0 ; i < database.size() ; i++){
+			
+			if(((String)temp[i]).lastIndexOf("*"))
+			
+		}
+		
 	}
 
 	@Override
@@ -53,6 +82,24 @@ public class VisitQueueDB implements VisitDAO{
 			throws NonExistingVisitException {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public String toString(){
+		
+		int size = database.size();
+		String toReturnString;
+		Object[] databaseArray = database.toArray();
+		
+		for(int i = 0 ; i < size ; i++){
+			
+			System.out.println(databaseArray[i]);
+			
+		}
+		
+		
+		// TODO
+		return "TODO";
 	}
 
 	
---
+++ ClinicSys/test/group1/clinic/data/VisitQueueDBTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package group1.clinic.data;
+
+import group1.util.ListUtilities;
+
+/**
+ * @author Danieil Skrinikov
+ *
+ */
+public class VisitQueueDBTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+	public static void toStringTest(){
+		
+		
+		
+	}
+	
+	private static void setup()
+	{
+	String[] visits = new String[4];
+	visits [0] = "SMIM85122501*2015*9*1*13*30*******";
+	visits [1] = "RODM90571001*2015*9*1*14*45*******";
+	visits [2] = "LISH87100101*2015*9*1*13*20*" +
+	"2015*12*1*13*45*2*Severe rash";
+	visits [3] = "RAOV86112001*2015*9*1*13*50*" +
+	"2015*12*1*14*10*5*Bored";
+	}
+
+}
