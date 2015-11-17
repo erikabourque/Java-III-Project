9b1580ece9943230d66743ae6cd622b9c5fece0e
Uen Yi Hung <uenyihung@gmail.com>
2015-11-17 17:25:30 -0500

    nothing, this is because I want to get rid of the you-have-yet-committed-me coloring. QQ all the way. *brawl*
    committing DawsonClinicPriorityPolicyTest thingy.

--- ClinicSys/test/group1/dawsonclinic/DawsonClinicPriorityPolicyTest.java
+++ ClinicSys/test/group1/dawsonclinic/DawsonClinicPriorityPolicyTest.java
@@ -1,13 +1,21 @@
 package group1.dawsonclinic;
 
 import java.awt.List;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.Queue;
+import static java.lang.System.out;
 
+import dw317.clinic.business.interfaces.Patient;
 import dw317.clinic.business.interfaces.PriorityPolicy;
+import dw317.clinic.business.interfaces.Visit;
+import group1.clinic.business.VisitSorter;
+import group1.clinic.data.ClinicFileLoader;
 import group1.clinic.data.ListPersistenceObject;
 import group1.clinic.data.SequentialTextFileList;
+import group1.clinic.data.VisitQueueDB;
 import group1.dawsonclinic.DawsonClinicPriorityPolicy;
+import group1.util.ListUtilities;
 import dw317.clinic.data.interfaces.VisitDAO;
 
 public class DawsonClinicPriorityPolicyTest {
@@ -16,14 +24,50 @@ public class DawsonClinicPriorityPolicyTest {
 		/**
 		 * The one parameter constructor will not be tested, since invalid
 		 * parameter will cause either compiler error or throw exception on the
-		 * application side, invalid data will not be able to be passed to the code.
+		 * application side, invalid data will not be able to be passed to the
+		 * code.
 		 */
-		
-		test1();
+
+		try {
+			test1();
+		} catch (IllegalArgumentException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	public static void test1(){
-		ListPersistenceObject aList = new SequentialTextFileList("datafiles/prioritypolicy/test1Patient.txt","datafiles/prioritypolicy/test1Visit.text");
-		VisitDAO visits = aList.getVisitDatabase());
+	public static void test1() throws IllegalArgumentException, IOException {
+		ListPersistenceObject aList = new SequentialTextFileList("datafiles/prioritypolicy/test1Patient.txt",
+				"datafiles/prioritypolicy/test1Visit.txt");
+		
+		Patient[] patientList = ClinicFileLoader
+				.getPatientListFromSequentialFile("datafiles/prioritypolicy/test1Patient.txt");
+		
+		ListUtilities.sort(patientList);
+		ListUtilities.saveListToTextFile(patientList, "datafiles/prioritypolicy/test1Patient.txt");
+		
+		Visit[] visitList = ClinicFileLoader.getVisitListFromSequentialFile("datafiles/prioritypolicy/test1Visit.txt", patientList);
+		VisitSorter sortVisit = new VisitSorter();
+		for (int x = 0; x < visitList.length - 1; x++) {
+			for (int y = x + 1; y < visitList.length; y++) {
+				if (sortVisit.compare((Visit) visitList[x], (Visit) visitList[y]) > 0) {
+					Visit temp = (Visit) visitList[y];
+					visitList[y] = visitList[x];
+					visitList[x] = temp;
+					temp = null;
+				}
+			}
+		}
+		ListUtilities.saveListToTextFile(visitList, "datafiles/prioritypolicy/test1Visit.txt");
+		VisitDAO visitsDB = new VisitQueueDB(aList);
+		
+		DawsonClinicPriorityPolicy dawsonPolicy = new DawsonClinicPriorityPolicy(visitsDB);
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit().get());
+		out.println(dawsonPolicy.getNextVisit());
 	}
 }
