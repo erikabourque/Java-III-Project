613090d9e29fe6de7fdd2e49837b8c3e16b9ab44
Uen Yi Hung <uenyihung@gmail.com>
2015-11-23 22:51:49 -0500

    I made the ObjectSerializedObjectTest output more readable

--- ClinicSys/test/group1/clinic/data/ObjectSerializedListTest.java
+++ ClinicSys/test/group1/clinic/data/ObjectSerializedListTest.java
@@ -12,45 +12,53 @@ import group1.clinic.business.ClinicPatient;
 import group1.clinic.business.ClinicVisit;
 import group1.util.ListUtilities;
 
+/**
+ * Test app for ObjectSerializedList class.
+ * 
+ * @author Katherine Richer 1434389
+ * @version 11-23-2015
+ *
+ */
 public class ObjectSerializedListTest {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		testTwoParamConstructor();
 		testGetPatientDatabase();
 		testGetVisitDatabase();
 		testSavePatientDatabase();
 		testSaveVisitDatabase();
-		
+
 	}
-	
-	public static void testGetPatientDatabase()
-	{
-		out.println("\nTesting the getPatientDatabase");
+
+	public static void testTwoParamConstructor() {
+		out.println("Testing the Two Param Constructor\n");
 		setup();
 
-		testGetPatientDatabase("Case 1 valid data",
-				"testfiles/testPatients.txt", "testfiles/testVisits.txt",
+		testTwoParamConstructor("Case 1: valid data", "testfiles/testPatients.txt", "testfiles/testVisits.txt",
 				DefaultPatientVisitFactory.DEFAULT, true);
-		testGetPatientDatabase("Case 2 null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT, false);
-		testGetPatientDatabase("Case 3 null DefaultPatientVisitFactory",
-			"testfiles/testPatients.txt", "testfiles/testVisits.txt", null, false);
-		testGetPatientDatabase("Case 4 all null data", null,null, null, false);
+		testTwoParamConstructor("Case 2: null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT,
+				false);
+		testTwoParamConstructor("Case 3: null DefaultPatientVisitFactory", "testfiles/testPatients.txt",
+				"testfiles/testVisits.txt", null, false);
+		testTwoParamConstructor("Case 4: all null data", null, null, null, false);
 
 		teardown();
+		out.println("*********************************************************************************************");
 	}
-	private  static void testGetPatientDatabase(String testcase, String patientFile,String visitFile,
-			DefaultPatientVisitFactory factory, boolean expectValid)
-	{
+
+	private static void testTwoParamConstructor(String testcase, String patientFile, String visitFile,
+			DefaultPatientVisitFactory factory, boolean expectValid) {
 		try {
 			out.println(testcase);
-			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser","datafiles/database/visits.ser");
+			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser",
+					"datafiles/database/visits.ser");
 			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
 			out.println("\tListObject creation success.");
-			
+
 			@SuppressWarnings("unused")
 			PatientListDB patients = new PatientListDB(listObject, factory);
 			out.println("\tConstructor success.");
-			out.println(listObject.getPatientDatabase());
+
 			if (!expectValid) {
 				out.println("Expected invalid ==== FAILED TEST ====");
 			}
@@ -64,36 +72,90 @@ public class ObjectSerializedListTest {
 			if (expectValid) {
 				out.println("Expected valid ==== FAILED TEST ====");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			out.println(
 					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 		}
+		out.println();
 	}
 	
-	public static void testGetVisitDatabase()
-	{
-		out.println("\nTesting the getVisitDatabase");
+	public static void testGetPatientDatabase() {
+		out.println("Testing the getPatientDatabase\n");
 		setup();
 
-		testGetVisitDatabase("Case 1 valid data",
-				"testfiles/testPatients.txt", "testfiles/testVisits.txt",
+		testGetPatientDatabase("Case 1: valid data", "testfiles/testPatients.txt", "testfiles/testVisits.txt",
 				DefaultPatientVisitFactory.DEFAULT, true);
-		testGetVisitDatabase("Case 2 null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT, false);
-		testGetVisitDatabase("Case 3 null DefaultPatientVisitFactory",
-			"testfiles/testPatients.txt", "testfiles/testVisits.txt", null, false);
-		testGetVisitDatabase("Case 4 all null data", null,null, null, false);
+		testGetPatientDatabase("Case 2: null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT,
+				false);
+		testGetPatientDatabase("Case 3: null DefaultPatientVisitFactory", "testfiles/testPatients.txt",
+				"testfiles/testVisits.txt", null, false);
+		testGetPatientDatabase("Case 4: all null data", null, null, null, false);
 
 		teardown();
+		out.println("*********************************************************************************************");
 	}
-	private static void testGetVisitDatabase(String testcase, String patientFile,String visitFile,
-			DefaultPatientVisitFactory factory, boolean expectValid)
-	{
+
+	private static void testGetPatientDatabase(String testcase, String patientFile, String visitFile,
+			DefaultPatientVisitFactory factory, boolean expectValid) {
 		try {
 			out.println(testcase);
-			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser","datafiles/database/visits.ser");
+			
+			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser",
+					"datafiles/database/visits.ser");
+			
 			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
 			out.println("\tListObject creation success.");
+
+			@SuppressWarnings("unused")
+			PatientListDB patients = new PatientListDB(listObject, factory);
+			out.println("\tConstructor success.");
+			out.println(listObject.getPatientDatabase());
 			
+			if (!expectValid) 
+				out.println("Expected invalid ==== FAILED TEST ====");
+			
+		} catch (IllegalArgumentException iae) {
+			out.println("\t" + iae.getMessage());
+			if (expectValid) {
+				out.println("Expected valid ==== FAILED TEST ====");
+			}
+		} catch (NullPointerException npe) {
+			out.println("\t" + npe.getMessage());
+			if (expectValid) {
+				out.println("Expected valid ==== FAILED TEST ====");
+			}
+		} catch (Exception e) {
+			out.println(
+					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+		}
+		out.println();
+	}
+
+	public static void testGetVisitDatabase() {
+		out.println("Testing the getVisitDatabase\n");
+		setup();
+
+		testGetVisitDatabase("Case 1: valid data", "testfiles/testPatients.txt", "testfiles/testVisits.txt",
+				DefaultPatientVisitFactory.DEFAULT, true);
+		testGetVisitDatabase("Case 2: null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT,
+				false);
+		testGetVisitDatabase("Case 3: null DefaultPatientVisitFactory", "testfiles/testPatients.txt",
+				"testfiles/testVisits.txt", null, false);
+		testGetVisitDatabase("Case 4: all null data", null, null, null, false);
+
+		teardown();
+		out.println("*********************************************************************************************");
+	}
+
+	private static void testGetVisitDatabase(String testcase, String patientFile, String visitFile,
+			DefaultPatientVisitFactory factory, boolean expectValid) {
+		try {
+			out.println(testcase);
+			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser",
+					"datafiles/database/visits.ser");
+			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
+			out.println("\tListObject creation success.");
+
 			@SuppressWarnings("unused")
 			PatientListDB patients = new PatientListDB(listObject, factory);
 			out.println("\tConstructor success.");
@@ -111,41 +173,43 @@ public class ObjectSerializedListTest {
 			if (expectValid) {
 				out.println("Expected valid ==== FAILED TEST ====");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			out.println(
 					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 		}
+		out.println();
 	}
-	public static void testSavePatientDatabase()
-	{
-		out.println("\nTesting the savePatientDatabase");
+
+	public static void testSavePatientDatabase() {
+		out.println("Testing the savePatientDatabase\n");
 		setup();
 
-		testSavePatientDatabase("Case 1 valid data",
-				"testfiles/testPatients.txt", "testfiles/testVisits.txt",
+		testSavePatientDatabase("Case 1: valid data", "testfiles/testPatients.txt", "testfiles/testVisits.txt",
 				DefaultPatientVisitFactory.DEFAULT, true);
-		testSavePatientDatabase("Case 2 null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT, false);
-		testSavePatientDatabase("Case 3 null DefaultPatientVisitFactory",
-			"testfiles/testPatients.txt", "testfiles/testVisits.txt", null, false);
-		testSavePatientDatabase("Case 4 all null data", null,null, null, false);
+		testSavePatientDatabase("Case 2: null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT,
+				false);
+		testSavePatientDatabase("Case 3: null DefaultPatientVisitFactory", "testfiles/testPatients.txt",
+				"testfiles/testVisits.txt", null, false);
+		testSavePatientDatabase("Case 4: all null data", null, null, null, false);
 
 		teardown();
-	
+		out.println("*********************************************************************************************");
+
 	}
-	
-	private static void testSavePatientDatabase(String testcase, String patientFile,String visitFile,
-			DefaultPatientVisitFactory factory, boolean expectValid)
-	{
+
+	private static void testSavePatientDatabase(String testcase, String patientFile, String visitFile,
+			DefaultPatientVisitFactory factory, boolean expectValid) {
 		try {
 			out.println(testcase);
-			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser","datafiles/database/visits.ser");
+			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser",
+					"datafiles/database/visits.ser");
 			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
 			out.println("\tListObject creation success.");
-			
+
 			@SuppressWarnings("unused")
 			PatientListDB patients = new PatientListDB(listObject, factory);
 			out.println("\tConstructor success.");
-			Patient aPatient = new ClinicPatient("David","An","ANDA94082123");
+			Patient aPatient = new ClinicPatient("David", "An", "ANDA94082123");
 			patients.add(aPatient);
 			patients.disconnect();
 			out.println(listObject.getPatientDatabase());
@@ -162,41 +226,43 @@ public class ObjectSerializedListTest {
 			if (expectValid) {
 				out.println("Expected valid ==== FAILED TEST ====");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			out.println(
 					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 		}
+		out.println();
 	}
-	public static void testSaveVisitDatabase()
-	{
-		out.println("\nTesting the saveVisitDatabase");
+
+	public static void testSaveVisitDatabase() {
+		out.println("Testing the saveVisitDatabase\n");
 		setup();
 
-		testSavePatientDatabase("Case 1 valid data",
-				"testfiles/testPatients.txt", "testfiles/testVisits.txt",
+		testSavePatientDatabase("Case 1: valid data", "testfiles/testPatients.txt", "testfiles/testVisits.txt",
 				DefaultPatientVisitFactory.DEFAULT, true);
-		testSavePatientDatabase("Case 2 null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT, false);
-		testSavePatientDatabase("Case 3 null DefaultPatientVisitFactory",
-			"testfiles/testPatients.txt", "testfiles/testVisits.txt", null, false);
-		testSavePatientDatabase("Case 4 all null data", null,null, null, false);
+		testSavePatientDatabase("Case 2: null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT,
+				false);
+		testSavePatientDatabase("Case 3: null DefaultPatientVisitFactory", "testfiles/testPatients.txt",
+				"testfiles/testVisits.txt", null, false);
+		testSavePatientDatabase("Case 4: all null data", null, null, null, false);
 
 		teardown();
-	
+		out.println("*********************************************************************************************");
 	}
-	
-	private static void testSaveVisitDatabase(String testcase, String patientFile,String visitFile,
-			DefaultPatientVisitFactory factory, boolean expectValid)
-	{
+
+	@SuppressWarnings("unused")
+	private static void testSaveVisitDatabase(String testcase, String patientFile, String visitFile,
+			DefaultPatientVisitFactory factory, boolean expectValid) {
 		try {
 			out.println(testcase);
-			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser","datafiles/database/visits.ser");
+			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser",
+					"datafiles/database/visits.ser");
 			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
 			out.println("\tListObject creation success.");
-			
+
 			@SuppressWarnings("unused")
 			VisitQueueDB visits = new VisitQueueDB(listObject, factory);
 			out.println("\tConstructor success.");
-			Patient aPatient = new ClinicPatient("David","An","ANDA94082123");
+			Patient aPatient = new ClinicPatient("David", "An", "ANDA94082123");
 			Visit aVisit = new ClinicVisit(aPatient);
 			visits.add(aVisit);
 			visits.disconnect();
@@ -214,104 +280,56 @@ public class ObjectSerializedListTest {
 			if (expectValid) {
 				out.println("Expected valid ==== FAILED TEST ====");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			out.println(
 					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 		}
+		out.println();
 	}
-	
 
-	
-	public static void testTwoParamConstructor() {
-		out.println("\nTesting the Two Param Constructor");
-		setup();
+	private static void setup() {
+		String[] patients = new String[8];
+		patients[0] = "LARR80072061*Renato*Laranja*5147341013*" + "DIN*00800430*Vancocin*Staph Infection";
+		patients[1] = "LISH87100101*Shao*Li**" + "DIN*02238645*292 tablets*Pain";
+		patients[2] = "RAOV86112001*Vishal*Rao*5143634564*" + "NDC*43479-501-51*Pimple punisher*Acne";
+		patients[3] = "RODM90571001*Maria*Rodriguez*5145555511****";
+		patients[4] = "SMIM85122501*Mike*Smith*5143634564*" + "DIN*02239497*Absorbine Jr*Athlete’s foot";
+		patients[5] = "SUCF34050513*Fernando*Sucre*5813085502****";
+		patients[6] = "TORD83511514*Diana*Torres*6379315732*" + "DIN*02204266*Dexamethasone-Omega*Asthma";
+		patients[7] = "WAKN60022987*Norio*Wakamoto*4389945870*"
+				+ "NDC*0363-8001-01*Nicotine Transdermal System*Smoking Addiction";
 
-		testTwoParamConstructor("Case 1 valid data",
-				"testfiles/testPatients.txt", "testfiles/testVisits.txt",
-				DefaultPatientVisitFactory.DEFAULT, true);
-		testTwoParamConstructor("Case 2 null SequentialTextFileList", null, null, DefaultPatientVisitFactory.DEFAULT, false);
-		testTwoParamConstructor("Case 3 null DefaultPatientVisitFactory",
-			"testfiles/testPatients.txt", "testfiles/testVisits.txt", null, false);
-		testTwoParamConstructor("Case 4 all null data", null,null, null, false);
+		String[] visits = new String[8];
+		visits[0] = "LARR80072061*2015*4*2*12*50*" + "2015*4*2*13*35*4*Stomach pain";
+		visits[1] = "LISH87100101*2015*9*1*13*20*" + "2015*12*1*13*45*2*Severe rash";
+		visits[2] = "RAOV86112001*2015*9*1*13*50*" + "2015*12*1*14*10*5*Bored";
+		visits[3] = "RODM90571001*2015*9*1*14*45*******";
+		visits[4] = "SMIM85122501*2015*9*1*13*30*******";
+		visits[5] = "SUCF34050513*2015*1*7*6*10*" + "2015*1*7*14*59*5*Finger injury";
+		visits[6] = "TORD83511514*2012*2*29*11*30*" + "2012*2*29*12*00*2*Asthmatic spasm";
+		visits[7] = "WAKN60022987*2014*12*31*23*52*" + "2015*1*1*00*08*1*Heart stroke";
 
-		teardown();
-	}
-	
-	
-
-	private static void testTwoParamConstructor(String testcase, String patientFile,String visitFile,
-			DefaultPatientVisitFactory factory, boolean expectValid) {
+		File dir = new File("testfiles");
 		try {
-			out.println(testcase);
-			ObjectSerializedList listObject = new ObjectSerializedList("datafiles/database/patients.ser","datafiles/database/visits.ser");
-			listObject.convertSequentialFilesToSerialized(patientFile, visitFile);
-			out.println("\tListObject creation success.");
-			
-			@SuppressWarnings("unused")
-			PatientListDB patients = new PatientListDB(listObject, factory);
-			out.println("\tConstructor success.");
-
-			if (!expectValid) {
-				out.println("Expected invalid ==== FAILED TEST ====");
+			if (!dir.exists()) {
+				dir.mkdirs();
 			}
-		} catch (IllegalArgumentException iae) {
-			out.println("\t" + iae.getMessage());
-			if (expectValid) {
-				out.println("Expected valid ==== FAILED TEST ====");
-			}
-		} catch (NullPointerException npe) {
-			out.println("\t" + npe.getMessage());
-			if (expectValid) {
-				out.println("Expected valid ==== FAILED TEST ====");
-			}
-		}catch (Exception e) {
-			out.println(
-					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+			ListUtilities.saveListToTextFile(patients, "testfiles/testPatients.txt");
+			ListUtilities.saveListToTextFile(visits, "testfiles/testVisits.txt");
+		} catch (IOException io) {
+			System.out.println("Error creating file in setUp()");
 		}
 	}
 
-private static void setup() {
-	String[] patients = new String[8];
-	patients[0] = "LARR80072061*Renato*Laranja*5147341013*" + "DIN*00800430*Vancocin*Staph Infection";
-	patients[1] = "LISH87100101*Shao*Li**" + "DIN*02238645*292 tablets*Pain";
-	patients[2] = "RAOV86112001*Vishal*Rao*5143634564*" + "NDC*43479-501-51*Pimple punisher*Acne";
-	patients[3] = "RODM90571001*Maria*Rodriguez*5145555511****";
-	patients[4] = "SMIM85122501*Mike*Smith*5143634564*" + "DIN*02239497*Absorbine Jr*Athlete’s foot";
-	patients[5] = "SUCF34050513*Fernando*Sucre*5813085502****";
-	patients[6] = "TORD83511514*Diana*Torres*6379315732*" + "DIN*02204266*Dexamethasone-Omega*Asthma";
-	patients[7] = "WAKN60022987*Norio*Wakamoto*4389945870*"
-			+ "NDC*0363-8001-01*Nicotine Transdermal System*Smoking Addiction";
-
-	String[] visits = new String[8];
-	visits[0] = "LARR80072061*2015*4*2*12*50*" + "2015*4*2*13*35*4*Stomach pain";
-	visits[1] = "LISH87100101*2015*9*1*13*20*" + "2015*12*1*13*45*2*Severe rash";
-	visits[2] = "RAOV86112001*2015*9*1*13*50*" + "2015*12*1*14*10*5*Bored";
-	visits[3] = "RODM90571001*2015*9*1*14*45*******";
-	visits[4] = "SMIM85122501*2015*9*1*13*30*******";
-	visits[5] = "SUCF34050513*2015*1*7*6*10*" + "2015*1*7*14*59*5*Finger injury";
-	visits[6] = "TORD83511514*2012*2*29*11*30*" + "2012*2*29*12*00*2*Asthmatic spasm";
-	visits[7] = "WAKN60022987*2014*12*31*23*52*" + "2015*1*1*00*08*1*Heart stroke";
-
-	File dir = new File("testfiles");
-	try {
-		if (!dir.exists()) {
-			dir.mkdirs();
+	private static void teardown() {
+		File theFile = new File("testfiles/testPatients.txt");
+		if (theFile.exists()) {
+			theFile.delete();
 		}
-		ListUtilities.saveListToTextFile(patients, "testfiles/testPatients.txt");
-		ListUtilities.saveListToTextFile(visits, "testfiles/testVisits.txt");
-	} catch (IOException io) {
-		System.out.println("Error creating file in setUp()");
+		theFile = new File("testfiles/testVisits.txt");
+		if (theFile.exists()) {
+			theFile.delete();
+		}
 	}
-}
-private static void teardown() {
-	File theFile = new File("testfiles/testPatients.txt");
-	if (theFile.exists()) {
-		theFile.delete();
-	}
-	theFile = new File("testfiles/testVisits.txt");
-	if (theFile.exists()) {
-		theFile.delete();
-	}
-}
 
 }
