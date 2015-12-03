f6e0a44c795118d2c991e29409e0d3cbf2134f29
Katherine Richer <katrich97@sympatico.ca>
2015-12-03 10:49:41 -0500

    set up the classes for part 2 (Text UI)

---
+++ ClinicSys/src/group1/clinic/DawsonClinicTextApp.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package group1.clinic;
+
+import dw317.clinic.ClinicFactory;
+import dw317.clinic.data.interfaces.PatientDAO;
+import dw317.clinic.data.interfaces.VisitDAO;
+import group1.clinic.business.Clinic;
+import group1.clinic.data.PatientListDB;
+import group1.clinic.data.VisitQueueDB;
+import group1.clinic.data.ObjectSerializedList;
+import group1.clinic.ui.TextController;
+import group1.clinic.ui.TextView;
+import group1.dawsonclinic.DawsonClinicFactory;
+/**
+ * @author Katherine Richer
+ *
+ */
+public class DawsonClinicTextApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClinicFactory factory =
+				DawsonClinicFactory.DAWSON_CLINIC;
+				PatientDAO patientDb =
+				new PatientListDB(new ObjectSerializedList
+				("datafiles/database/patients.ser" ,
+				"datafiles/database/visits.ser"),
+				factory.getPatientVisitFactory());
+				VisitDAO visitDb =
+				new VisitQueueDB(new ObjectSerializedList
+				("datafiles/database/patients.ser" ,
+				"datafiles/database/visits.ser"),
+				factory.getPatientVisitFactory());
+				Clinic model = new Clinic(patientDb, visitDb, factory);
+				TextView view = new TextView(model);
+				TextController controller = new TextController(model);
+				controller.run();
+
+	}
+
+}
---
+++ ClinicSys/src/group1/clinic/ui/TextView.java
@@ -0,0 +1,22 @@
+package group1.clinic.ui;
+
+import group1.clinic.business.Clinic;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class TextView implements Observer {
+
+
+	
+	public TextView(Observable model)
+	{
+		
+	}
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
