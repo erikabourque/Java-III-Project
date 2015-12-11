e3df9d8426907f089920e39d964d53238b37638f
Erika Bourque <erika.bourque94@gmail.com>
2015-12-11 18:25:15 -0500

    TextController - added getPatient and addPatient

--- ClinicSys/src/group1/clinic/ui/TextController.java
+++ ClinicSys/src/group1/clinic/ui/TextController.java
@@ -8,8 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import static java.lang.System.out;
 
+import dw317.clinic.business.interfaces.Patient;
 import dw317.clinic.business.interfaces.PatientVisitManager;
+import dw317.clinic.data.DuplicatePatientException;
+import dw317.clinic.data.NonExistingPatientException;
 
 /**
  * Controller for the TextApp for a PatientVisitManager.  Provides a text-based menu that allows a person to
@@ -31,11 +35,21 @@ public class TextController {
 		NEXT_TO_EXAMINE, STOP
 		}
 		
+		/**
+		 * One Param Constructor.  Assigns given PatientVisitManager as
+		 * the model to use.
+		 * 
+		 * @param model	The PatientVisitManager to be used.
+		 */
 		public TextController(PatientVisitManager model)
 		{
 			this.model = model;
 		}
 		
+		/**
+		 * Method used to start interacting with the text-based interface.
+		 * Loops until user specifies to stop.
+		 */
 		public void run()
 		{
 			Command userCommand = null;
@@ -43,11 +57,32 @@ public class TextController {
 			while (userCommand != Command.STOP)
 			{
 				printMenu();
-				// Getting user input
+				
+				// Getting user input				
 				userCommand = getCommand();
+				
+				// Take action depending on that input
+				if (userCommand == Command.PATIENT_INFO)
+				{
+					getPatient();
+				}
+				
+				if (userCommand == Command.NEW_PATIENT)
+				{
+					addPatient();
+				}
+				
+				// Start a newline after any commands
+				out.println();
 			}			
 		}
 		
+		/**
+		 * Gets the user's command through their input.  Checks to make sure
+		 * input is valid.
+		 * 
+		 * @return	The command that was chosen by the user's input
+		 */
 		private Command getCommand()
 		{
 			int commandNum = 0;
@@ -58,11 +93,11 @@ public class TextController {
 			}
 			catch (NumberFormatException nfe)
 			{
-				System.out.println("Please enter an integer only.");
+				System.out.println("Please enter one digit only.");
 			}
 			catch (IOException ioe)
 			{
-				System.out.println("Please enter an integer only.");
+				System.out.println(ioe.getMessage());
 			}
 			
 			switch(commandNum)
@@ -86,18 +121,130 @@ public class TextController {
 			}
 		}
 		
+		/**
+		 * Prints the text-based menu.
+		 */
 		private void printMenu()
 		{
-			System.out.println("Dawson Clinic Menu");
-			System.out.println("Select a choice from the menu:");
-			System.out.println("\t1 - PATIENT_INFO");
-			System.out.println("\t2 - NEW_PATIENT");
-			System.out.println("\t3 - NEW_VISIT");
-			System.out.println("\t4 - NEXT_TO_TRIAGE");
-			System.out.println("\t5 - CHANGE_PRIORITY");
-			System.out.println("\t6 - NEXT_TO_EXAMINE");
-			System.out.println("\t7 - STOP");
-			System.out.print("\nEnter your choice: ");
+			out.println("Dawson Clinic Menu");
+			out.println("Select a choice from the menu:");
+			out.println("\t1 - PATIENT_INFO");
+			out.println("\t2 - NEW_PATIENT");
+			out.println("\t3 - NEW_VISIT");
+			out.println("\t4 - NEXT_TO_TRIAGE");
+			out.println("\t5 - CHANGE_PRIORITY");
+			out.println("\t6 - NEXT_TO_EXAMINE");
+			out.println("\t7 - STOP");
+			out.print("\nEnter your choice: ");			
+		}
+		
+		/**
+		 * Retrieves the patient with the ramq specified 
+		 * by the user's input.
+		 */
+		private void getPatient()
+		{
+			String ramq;
+			Patient thePatient;
+			
+			// Request and get the RAMQ
+			out.print("Please enter the ramq: ");
+			try
+			{
+				ramq = userInput.readLine();
+				thePatient = model.findPatient(ramq);
+				
+				// Print the resulting patient
+				out.println("Patient information");
+				out.println("RAMQ: " + thePatient.getRamq());
+				out.println(thePatient.getName().getLastName() + ", " + thePatient.getName().getFirstName());
+				out.println("Telephone: " + thePatient.getTelephoneNumber());
+			} 
+			catch (IllegalArgumentException iae)
+			{
+				out.println(iae.getMessage());
+			} 
+			catch (IOException ioe) 
+			{
+				out.println(ioe.getMessage());
+			} 
+			catch (NonExistingPatientException nepe) 
+			{
+				out.println(nepe.getMessage());
+			}			
+		}
+		
+		/**
+		 * Adds a new patient to the database.  Requests Information
+		 * from the user, and then creates the patient.
+		 */
+		private void addPatient()
+		{
+			String fname;
+			String lname;
+			String ramq;
+			String phone;
+			String existingMedCond;
+
+			try
+			{
+				// Requests each piece of information about the patient and saves them
+				out.print("Please enter the first name: ");
+				fname = userInput.readLine();
+				
+				out.print("Please enter the last name: ");
+				lname = userInput.readLine();
+				
+				out.print("Please enter the ramq: ");
+				ramq = userInput.readLine();
+				
+				out.print("Please enter the phone number: ");
+				phone = userInput.readLine();
+				
+				out.print("Please enter any existing medical conditions, or "
+						+ "press enter if none available: ");
+				existingMedCond = userInput.readLine();
+				
+				// Check if user had any to input, make it null
+				if (existingMedCond.isEmpty())
+				{
+					existingMedCond = null;
+				}				
+				
+				// Create the patient
+				model.registerNewPatient(fname, lname, ramq, phone, null, existingMedCond);				
+			}
+			catch (IOException ioe)
+			{
+				out.println(ioe.getMessage());
+			}
+			catch (DuplicatePatientException dpe) 
+			{
+				out.println(dpe.getMessage());
+			}
+			catch (IllegalArgumentException iae)
+			{
+				out.println(iae.getMessage());
+			}
+		}
+		
+		private void addVisit()
+		{
+			
+		}
+		
+		private void getTriageVisit()
+		{
+			
+		}
+		
+		private void changePriority()
+		{
+			
+		}
+		
+		private void getExaminationVisit()
+		{
 			
 		}
 }
